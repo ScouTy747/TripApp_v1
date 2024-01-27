b48b0f53369257e21396bd4710213077f7a019ba
ScouTy747 <73474180+ScouTy747@users.noreply.github.com>
2024-01-27 22:20:16 +0100

    up

--- TripAppFrontend/TripAppFrontend/obj/Debug/net8.0-ios/iossimulator-x64/TripAppFrontend.AssemblyInfo.cs
+++ TripAppFrontend/TripAppFrontend/obj/Debug/net8.0-ios/iossimulator-x64/TripAppFrontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TripAppFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+70ccf07e140fe5ae60db0aac76040423d8c81b44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b50ab4ad757324d27dbb04b6b6e8c43130da0c1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TripAppFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TripAppFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
